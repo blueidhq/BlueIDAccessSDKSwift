502c9c8e5fb0815196bd5ae67dce5a6e96c78a84
blueidhq <alexander.adam@blueid.net>
2023-12-08 19:04:52 +0000

    v0.32.0

--- Tests/BlueIDAccessSDKTests/CommandsTests.swift
+++ Tests/BlueIDAccessSDKTests/CommandsTests.swift
@@ -2,10 +2,8 @@ import XCTest
 
 @testable import BlueIDAccessSDK
 
-final class CommandsTests: XCTestCase {
+final class CommandsTests: BlueXCTestCase {
     func testRunCommand() async throws {
-        _ = try await blueRunCommand("initialize")
-        
         let commandResult: BlueCommandResult = try await blueRunCommand("versionInfo")
         
         XCTAssertEqual(commandResult.messageTypeName, "BlueVersionInfo")
@@ -18,5 +16,23 @@ final class CommandsTests: XCTestCase {
         let versionInfo: BlueVersionInfo = try blueDecodeMessage(versionInfoData)
         
         XCTAssertEqual(versionInfo.version, 1);
+    }
+    
+    func testRunGetAccessCredentialsCommand() async throws {
+        let arg0: Any? = NSNull()
+        let arg1: Data? = nil
+        
+        let commandResult: BlueCommandResult = try await blueRunCommand("getAccessCredentials", arg0: arg0, arg1: arg1)
+        
+        XCTAssertEqual(commandResult.messageTypeName, "BlueAccessCredentialList")
+        
+        guard let credentialListData = commandResult.data as? Data else {
+            XCTAssert(false)
+            return
+        }
+        
+        let credentialList: BlueAccessCredentialList = try blueDecodeMessage(credentialListData)
+        
+        XCTAssertNotNil(credentialList)
     }
 }
