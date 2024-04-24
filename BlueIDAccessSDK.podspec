4e2f133c2d38f822fbc42df0a00bfa60467f7157
blueidhq <alexander.adam@blueid.net>
2024-04-24 18:36:49 +0000

    v1.21.0

--- BlueIDAccessSDK.podspec
+++ BlueIDAccessSDK.podspec
@@ -1,23 +1,10 @@
 Pod::Spec.new do |s|
-  s.name = 'BlueIDAccessSDK'
-  s.version = '1.19.0'
-  s.license = { :type => 'MIT' }
-  s.summary = 'BlueID Access SDK for Swift'
-  s.homepage = 'https://www.blue-id.com'
-  s.author = 'BlueID GmbH'
-  s.source = { :git => 'https://github.com/blueidhq/BlueIDAccessSDKSwift.git', :tag => 'v' + s.version.to_s }
-
-  s.requires_arc = true
-  s.ios.deployment_target = '14.0'
-  # s.osx.deployment_target = '10.13'
-  # s.watchos.deployment_target = '8.0'
-
-  s.source_files = 'Sources/BlueIDAccessSDK/**/*.swift'
-
-  s.vendored_frameworks = 'CBlueIDAccess.xcframework'
-
-  s.swift_version = '5.8'
-
-  s.dependency 'SwiftProtobuf', '1.24.0'
-  s.dependency 'iOSDFULibrary', '4.15.0'
-end
+    s.name                = 'BlueIDAccessSDK'
+    s.version             = '1.0.0'
+    s.summary             = 'BlueID Access SDK'
+    s.homepage            = 'https://www.blue-id.com'
+    s.license             = { :type => 'MIT' }
+    s.author              = { 'BlueIDAccessSDK' => 'BlueID GmbH' }
+    s.source              = { :http => "file:///Users/Downloads/Frameworks.zip" }
+    s.vendored_frameworks = 'BlueIDAccessSDK.xcframework'
+  end
--- Package.swift
+++ Package.swift
@@ -1,40 +1,36 @@
-// swift-tools-version:5.8
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "BlueIDAccessSDK",
-  platforms: [
-    .macOS(.v12),
-    .iOS(.v14),
-    .watchOS(.v8),
-  ],
-  products: [
-    .library(
-      name: "BlueIDAccessSDK",
-      targets: ["BlueIDAccessSDK", "CBlueIDAccess"]
-    )
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.24.0"),
-    .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", from: "4.15.0")
-  ],
-  targets: [
-    .target(
-      name: "BlueIDAccessSDK",
-      dependencies: [
-        "CBlueIDAccess",
-        .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-        .product(name: "NordicDFU", package: "IOS-DFU-Library")
-      ]
-    ),
-    .binaryTarget(
-      name: "CBlueIDAccess",
-      path: "./CBlueIDAccess.xcframework"
-    ),
-    .testTarget(
-      name: "BlueIDAccessSDKTests",
-      dependencies: ["BlueIDAccessSDK"]
-    ),
-  ]
+    name: "BlueIDAccessSDK",
+    platforms: [
+      .macOS(.v12),
+      .iOS(.v14),
+      .watchOS(.v8),
+    ],
+    products: [
+        .library(
+            name: "BlueIDAccessSDK",
+            targets: ["BlueIDAccessSDKLib", "BlueIDAccessSDKFramework"]
+        ),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.24.0"),
+      .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", from: "4.15.0")
+    ],
+    targets: [
+        .target(
+            name: "BlueIDAccessSDKLib",
+            dependencies: [
+              .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+              .product(name: "NordicDFU", package: "IOS-DFU-Library")
+            ]
+        ),
+        .binaryTarget(
+            name: "BlueIDAccessSDKFramework",
+            path: "./BlueIDAccessSDK.xcframework"
+        )
+    ]
 )
