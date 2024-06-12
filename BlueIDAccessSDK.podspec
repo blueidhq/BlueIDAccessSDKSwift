431270555bc61b712329218075ba90753f86e3cc
blueidhq <alexander.adam@blueid.net>
2024-06-12 13:35:24 +0000

    v1.61.0

--- BlueIDAccessSDK.podspec
+++ BlueIDAccessSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
     s.name = 'BlueIDAccessSDK'
-    s.version = '1.59.0'
+    s.version = '1.61.0'
     s.summary = 'BlueID Access SDK'
     s.homepage = 'https://www.blue-id.com'
     s.license = { :type => 'MIT' }
@@ -12,4 +12,5 @@ Pod::Spec.new do |s|
     s.swift_version = '5.8'
     s.dependency 'SwiftProtobuf', '1.24.0'
     s.dependency 'iOSDFULibrary', '4.15.0'
+    s.dependency 'Sentry', '8.28.0'
   end
--- Package.swift
+++ Package.swift
@@ -18,14 +18,16 @@ let package = Package(
     ],
     dependencies: [
       .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.24.0"),
-      .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", from: "4.15.0")
+      .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", from: "4.15.0"),
+      .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.28.0")
     ],
     targets: [
         .target(
             name: "BlueIDAccessSDKLib",
             dependencies: [
               .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-              .product(name: "NordicDFU", package: "IOS-DFU-Library")
+              .product(name: "NordicDFU", package: "IOS-DFU-Library"),
+              .product(name: "Sentry", package: "sentry-cocoa")
             ]
         ),
         .binaryTarget(
