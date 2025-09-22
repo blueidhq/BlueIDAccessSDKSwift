96bf439785ffeabad6d62c216335c75c1e04cc05
BlueID <apps@blue-id.com>
2025-09-22 12:01:43 +0000

    v11.4.0

---
+++ BlueIDAccessSDK.xcframework/macos-arm64_x86_64/BlueIDAccessSDK.framework/Versions/Current/Headers/BlueIDAccessSDKFramework.h
@@ -0,0 +1,18 @@
+//
+//  BlueIDAccessSDKFramework.h
+//  BlueIDAccessSDKFramework
+//
+//  Created by Matheus Netto on 22/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlueIDAccessSDKFramework.
+FOUNDATION_EXPORT double BlueIDAccessSDKFrameworkVersionNumber;
+
+//! Project version string for BlueIDAccessSDKFramework.
+FOUNDATION_EXPORT const unsigned char BlueIDAccessSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueIDAccessSDKFramework/PublicHeader.h>
+
+
